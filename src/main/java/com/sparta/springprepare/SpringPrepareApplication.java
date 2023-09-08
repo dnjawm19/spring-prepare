9cc709e1216b992d8be2287bcb9d943641e81181
jeongsoo <dnjawm19@naver.com>
2023-09-08 19:58:50 +0900

    create api

--- src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
+++ src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPrepareApplication {
+public class  SpringPrepareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPrepareApplication.class, args);
